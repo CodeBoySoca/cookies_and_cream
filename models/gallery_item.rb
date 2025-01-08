6c300bf3bec519bc1c5382dad0e2705b55a25be6
Rocky Latchman <kidmongoose@gmail.com>
2025-01-08 01:51:30 -0800

    Method stubs for Gallery Item class

---
+++ models/gallery_item.rb
@@ -0,0 +1,17 @@
+
+class GalleryItem < ActiveRecord::Base
+  def add_item(item)
+  end
+
+  def edit_item(item_id)
+  end
+
+  def get_item(item_id)
+  end
+
+  def get_all_items(menu_id)
+  end
+
+  def remove_item(item_id)
+  end
+end
--- models/item_gallery.rb
+++
@@ -1,4 +0,0 @@
-
-class ItemGallery < ActiveRecord::Base
-
-end
