ef434d455b447e4d72891946b813059199864bea
Rocky Latchman <kidmongoose@gmail.com>
2025-01-08 01:31:34 -0800

    Method stubs for Review class

--- models/review.rb
+++ models/review.rb
@@ -1,2 +1,17 @@
 class Review < ActiveRecord::Base
+  def add_review()
+  end
+
+  def retrieve_reviews()
+  end
+
+  def retrieve_review()
+  end
+
+  def edit_review(review_id)
+  end
+
+  def remove_review()
+  end
+
 end
