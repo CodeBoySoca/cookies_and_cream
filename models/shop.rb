d09f5f5f287c34efa3f90a0969773b3a67e90b02
Rocky Latchman <kidmongoose@gmail.com>
2025-01-08 01:02:55 -0800

    Method stubs for Shop class

--- models/shop.rb
+++ models/shop.rb
@@ -1,4 +1,18 @@
 
 class Shop < ActiveRecord::Base
+  def add_shop()
+  end
+
+  def edit_shop()
+  end
+
+  def remove_shop()
+  end
+
+  def retrieve_shop()
+  end
+
+  def retrieve_shops()
+  end
 
 end
