efa038ba8c63437b369685c35864b093086aa0a1
Rocky Latchman <kidmongoose@gmail.com>
2025-01-08 00:52:25 -0800

    Method stubs for Favorite class

--- models/favorite.rb
+++ models/favorite.rb
@@ -1,2 +1,20 @@
 class Favorite < ActiveRecord::Base
+  def add_favorite()
+  end
+
+  def edit_favorite()
+  end
+
+  def remove_favorite()
+  end
+
+  def retrieve_favorite()
+  end
+
+  def retrieve_favorites()
+  end
+
+  def toggle_visibility()
+    #change public visibility of your favorites
+  end
 end
