1a1bce57e23a2ded1ad572e0d6cf9d7772865ecf
Rocky Latchman <kidmongoose@gmail.com>
2025-01-08 01:48:38 -0800

    Method stubs for Menu Item class

--- models/menu_item.rb
+++ models/menu_item.rb
@@ -1,2 +1,16 @@
 class MenuItem < ActiveRecord::Base
+  def add_item(item)
+  end
+
+  def edit_item(item_id)
+  end
+
+  def get_item(item_id)
+  end
+
+  def get_all_items(menu_id)
+  end
+
+  def remove_item(item_id)
+  end
 end
