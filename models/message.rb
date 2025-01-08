ede264fff348930723f27a515bb4ad0c2a0e2b1a
Rocky Latchman <kidmongoose@gmail.com>
2025-01-08 01:44:46 -0800

    Method stubs for Message class

--- models/message.rb
+++ models/message.rb
@@ -1,2 +1,17 @@
 class Message < ActiveRecord::Base
+  def retrieve_message()
+  end
+
+  def retrieve_messages(recipient_id)
+  end
+
+  def send_message(sender_id, recipient_id)
+  end
+
+  def delete_message()
+  end
+
+  def delete_all_message()
+  end
+
 end
