6f3dfa24d526eda153e0a6bcd52c03b5451b72c9
Rocky Latchman <kidmongoose@gmail.com>
2025-01-09 17:38:21 -0800

    Changed the name of some for the routes and added others for the project

--- app.rb
+++ app.rb
@@ -12,6 +12,7 @@ enable :sessions
 
 class CookiesCreamApp < Sinatra::Base
 
+  #global
   get '/' do
     #a landing page?
   end
@@ -19,11 +20,28 @@ class CookiesCreamApp < Sinatra::Base
   get '/signout' do
   end
 
-  get '/signin' do
+  get '/account/type' do
+    erb :account_type
+  end
+
+  patch '/settings' do
+
+  end
+
+  get '/settings/' do
+
+  end
+
+  #users
+  get '/signup' do
+    erb :signup
+  end
+
+  get '/email/magic-link' do
     erb :signin
   end
 
-  post '/email/sent' do
+  get '/send/magic-link' do
     #page to display to the user to tell them to check their e-mail
     #send email with magic link
     email = Email.new
@@ -46,7 +64,7 @@ class CookiesCreamApp < Sinatra::Base
     erb :email_sent
   end
 
-  get '/verify' do
+  get '/verify/account' do
     #verify the users token
     email = Email.new
     decoded_token = email.verify_token(params[:token])
@@ -58,7 +76,13 @@ class CookiesCreamApp < Sinatra::Base
     end
   end
 
-  get '/profile/setup' do
+  get '/location' do
+  end
+
+  get '/profile-image' do
+  end
+
+  get '/dessert/favs' do
   end
 
   get '/shops' do
@@ -69,23 +93,11 @@ class CookiesCreamApp < Sinatra::Base
 
   end
 
-  get '/shop/:shop_name' do
-
-  end
-
   get '/messages/' do
 
   end
 
   post '/messages' do
-
-  end
-
-  get '/settings' do
-
-  end
-
-  patch '/settings' do
 
   end
 
@@ -128,46 +140,88 @@ class CookiesCreamApp < Sinatra::Base
 
  end
 
-  get '/register/shop' do
+
+  #shops
+  get '/shop/signup' do
+     erb :signup
+  end
+
+  get '/shop/email/magic-link' do
+    erb :signin
+  end
+
+  get '/shop/send/magic-link' do
+    #page to display to the user to tell them to check their e-mail
+    #send email with magic link
+    email = Email.new
+    token = email.generate_token(params[:email])
+    magic_link = "http://localhost:4567/verify?token=#{token}"
+    email.send_magic_link({
+        :to =>  ENV['EMAIL_RECIPIENT'],
+        :via => :smtp,
+        :via_options => {
+        :address =>  ENV['EMAIL_SERVER'],
+          :port =>  ENV['EMAIL_PORT'],
+          :enable_starttls_auto =>  true,
+          :user_name =>  ENV['EMAIL_SENDER'],
+          :password =>  ENV['EMAIL_PASSWORD']
+        },
+        :subject => 'Cookies and Cream - Magic Link',
+        :html_body => ERB.new(File.read('views/magic_link.erb')).result_with_hash({:name => 'Joe', :magic_link => magic_link}),
+        :body => 'Hello #{data["name"]} click the link to sign in <a href="#{data["magic_link"]}">Sign in</a>'
+      })
+    erb :email_sent
+  end
+
+  get '/shop/verify/account' do
+    #verify the users token
+    email = Email.new
+    decoded_token = email.verify_token(params[:token])
+    if decoded_token
+       #save to database and create a session
+       redirect :shops
+    else
+      'Invalid or expired token'
+    end
+  end
+
+  get '/shop/owner/profile' do
 
   end
 
-  post '/register/shop' do
+  get '/shop/add' do
 
+  end
+
+  post '/shop/add' do
+  end
+
+  get '/shop/add/banner' do
+  end
+
+  patch '/shop/add/banner' do
+  end
+
+  get '/shop/:shop_name' do
+  end
+
+  get '/shop/edit/:shop_name' do
+  end
+
+  get '/shop/remove/:shop_name' do
   end
 
   get '/shops/:account_id' do
+  end
+
+  get '/shop/reviews/:shop_name' do
+  end
+
+  get '/shop/messages/:shop_name' do
 
   end
 
-  post '/shops/:account_id' do
-
-  end
-
-  get '/settings/:account_id' do
-
-  end
-
-  patch '/settings/:account_id' do
-
-  end
-
-  patch '/shop/edit/:shop_id' do
-
-  end
-
-  delete '/shop/remove/:shop_id' do
-  end
-
-  get '/shop/reviews/:shop_id' do
-
-  end
-
-  get '/shop/messages/:shop_id' do
-
-  end
-
-  post '/shop/:shop_id/message/:user_id' do
+  post '/shop/:shop_name/message/:user_id' do
   end
 
   run!
